867aa8c8c19fa724a6e45b188c29cad53dc23cdc
notnahack127 <notnahack@gmail.com>
2026-09-18 02:25:29 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,13 +1,4 @@
-// ==========================================
 // KAS WARGA RT 08 - SUPABASE CONFIG
-// ==========================================
-
 const SUPABASE_URL = "https://frosvokexwlbfucfkqli.supabase.co";
-
-// Tempel PUBLISHABLE KEY SUPABASE LENGKAP di antara tanda kutip
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_H5f_s1Cl6dPqtFalsA4f-A_ebX6J4zA";
-
-const sb = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_PUBLISHABLE_KEY
-);
+const sb = supabase.createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
